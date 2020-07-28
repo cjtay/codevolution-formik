0df790f060f0368c8db732b874513a968ea12d40
Tay CJ <cjtay@outlook.sg>
2020-07-28 19:51:49 +0800

    imac tutorial 5 handling form submission

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -8,38 +8,41 @@ function YoutubeForm() {
             email: '',
             channel: '',
         },
+        onSubmit: (values) => {
+            console.log('Form data', values);
+        },
     });
 
-    console.log('Form values', formik.values);
+    // console.log('Form values', formik.values);
 
     return (
         <div>
-            <form>
-                <label htmlFor='name'>Name</label>
+            <form onSubmit={formik.handleSubmit}>
+                <label htmlFor="name">Name</label>
                 <input
-                    type='text'
-                    id='name'
-                    name='name'
+                    type="text"
+                    id="name"
+                    name="name"
                     onChange={formik.handleChange}
                     value={formik.values.name}
                 />
-                <label htmlFor='email'>Email</label>
+                <label htmlFor="email">Email</label>
                 <input
-                    type='email'
-                    id='email'
-                    name='email'
+                    type="email"
+                    id="email"
+                    name="email"
                     onChange={formik.handleChange}
                     value={formik.values.email}
                 />
-                <label htmlFor='channel'>Channel</label>
+                <label htmlFor="channel">Channel</label>
                 <input
-                    type='text'
-                    id='channel'
-                    name='channel'
+                    type="text"
+                    id="channel"
+                    name="channel"
                     onChange={formik.handleChange}
                     value={formik.values.channel}
                 />
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </div>
     );
