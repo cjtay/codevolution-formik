7d64ff7b6449a2d4143c2537b9c6b65b331ef4ae
Tay CJ <cjtay@outlook.sg>
2020-08-04 16:32:22 +0800

    imac tutorial 8 - displaying error messages

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,47 +1,92 @@
 import React from 'react';
 import { useFormik } from 'formik';
 
+const initialValues = {
+    name: 'Tay',
+    email: '',
+    channel: '',
+};
+
+const onSubmit = (values) => {
+    console.log('Form data', values);
+};
+
+const validate = (values) => {
+    let errors = {};
+
+    if (!values.name) {
+        errors.name = 'Required';
+    }
+
+    if (!values.email) {
+        errors.email = 'Required';
+    } else if (
+        !/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+            values.email
+        )
+    ) {
+        errors.email = 'Enter valid email';
+    }
+    if (!values.channel) {
+        errors.channel = 'Required';
+    }
+
+    return errors;
+};
+
 function YoutubeForm() {
     const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: '',
-        },
-        onSubmit: (values) => {
-            console.log('Form data', values);
-        },
+        initialValues,
+        onSubmit,
+        validate,
     });
 
-    // console.log('Form values', formik.values);
+    console.log('Form errors', formik.errors);
 
     return (
         <div>
             <form onSubmit={formik.handleSubmit}>
-                <label htmlFor="name">Name</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    onChange={formik.handleChange}
-                    value={formik.values.name}
-                />
-                <label htmlFor="email">Email</label>
-                <input
-                    type="email"
-                    id="email"
-                    name="email"
-                    onChange={formik.handleChange}
-                    value={formik.values.email}
-                />
-                <label htmlFor="channel">Channel</label>
-                <input
-                    type="text"
-                    id="channel"
-                    name="channel"
-                    onChange={formik.handleChange}
-                    value={formik.values.channel}
-                />
+                <div className="form-control">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                    />
+                    {formik.errors.name ? (
+                        <div className="error">{formik.errors.name}</div>
+                    ) : null}
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        onChange={formik.handleChange}
+                        value={formik.values.email}
+                    />
+                    {formik.errors.email ? (
+                        <div className="error">{formik.errors.email}</div>
+                    ) : null}
+                </div>
+
+                <div className="form-control">
+                    <label htmlFor="channel">Channel</label>
+                    <input
+                        type="text"
+                        id="channel"
+                        name="channel"
+                        onChange={formik.handleChange}
+                        value={formik.values.channel}
+                    />
+                    {formik.errors.channel ? (
+                        <div className="error">{formik.errors.channel}</div>
+                    ) : null}
+                </div>
                 <button type="submit">Submit</button>
             </form>
         </div>
